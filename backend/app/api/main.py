a3475298d603746f7b3033bfc0c3c80b3d7df889
pascal918 <pascal.kai98@outlook.com>
2024-10-13 22:55:23 +0200

    add login router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.routes import calculate
+from app.api.routes import items, calculate, login, users,utils
 
 api_router = APIRouter()
 api_router.include_router(calculate.router, prefix="/calculate", tags=["calculate"])
+api_router.include_router(login.router, tags=["login"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+api_router.include_router(items.router, prefix="/items", tags=["items"])
+
